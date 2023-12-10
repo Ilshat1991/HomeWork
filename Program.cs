9981f351314533cddd91293d31bac8bbfbf54fdb
Ilshat <huzin.ilshat.91@mail.ru>
2023-12-10 07:44:08 +0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿//Задача №1.
+
+// void FillArray (int[] Arr)
+// {
+//     for (int i = 0; i<=9; i++)
+//     {
+//         Arr[i]= new Random().Next(1,100);
+//         Console.Write (Arr[i]+" ");
+//     }
+// }
+
+// int [] array = new int [10];
+// FillArray(array);
+// int count=0;
+// for (int i=0; i<10; i++)
+// {
+//     if (array[i]<=90 && array[i]>=20) count++;
+// }
+// Console.WriteLine();
+// Console.Write(count);
+
+
+
+
+//Задача №2.
+
+// void FillArray (int[] Arr)
+// {
+//     for (int i = 0; i<=9; i++)
+//     {
+//         Arr[i]= new Random().Next(1,100);
+//         Console.Write (Arr[i]+" ");
+//     }
+// }
+
+// int [] array = new int [10];
+// FillArray(array);
+// int count=0;
+// for (int i=0; i<10; i++)
+// {
+//     if (array[i]%2==0) count++;
+// }
+// Console.WriteLine();
+// Console.Write(count);
+
+
+
+//Задача №3
+
+// void FillArray (double[] Arr)
+// {
+//     for (int i = 0; i<=9; i++)
+//     {
+//         Arr[i]= new Random().Next(100,10000)*0.01 ;
+
+//         Console.Write (Arr[i]+" ");
+//     }
+// }
+
+// double [] array = new double [10];
+// FillArray(array);
+// double max=array[0];
+// double min=array[0];
+// for (int i=0; i<10; i++)
+// {
+//     if (array[i] > max) max=array[i];
+//     else if (array[i]<min) min= array[i];
+// }
+// double delta = max - min;
+// Console.WriteLine();
+// Console.WriteLine(max+" "+min+" "+delta);
